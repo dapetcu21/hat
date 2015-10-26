64bc4e0fbd140fb3ec9df7da54588137843e04d3
Bogdan <bogdan@bit-q.com>
2015-10-26 18:28:29 +0200

    completed client.js / don't know if it works

--- Contents/Client/client.js
+++ Contents/Client/client.js
@@ -1,11 +1,38 @@
 (function () {
   room = new MAF.Room();
+  var player = null;
+  var color = [
+    "red",
+    "blue",
+    "green",
+    "yellow"
+  ];
 
   room.addEventListener('joined', function (event) {
     console.log('user joined', event.user);
   });
 
   room.addEventListener('data', function (event) {
+    var d = event.data;
+
+    if (d.h === room.hash) {
+      player = d.r;
+      document.getElementById("player").className = document.getElementById("player").className + "player-" + color[0];
+    }
+
     console.log('data', event.data);
   });
+
+  button.on('touchstart', function () {
+    room.send({ a: 'bd', b: 'a', p: player });  
+  });
+
+  button.on('touchmove', function () {
+    room.send({ a: 'bm', b: 'b', p: player });
+  });
+
+  button.on('touchend', function () {
+    room.send({ a: 'bu', b: 'a', p: player });
+  });
+  
 }());
